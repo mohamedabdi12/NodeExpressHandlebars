9e0152463e650b88f370dfc906732a171858abf9
Mohamed Abdi <lovegoldone@gmail>
2020-08-27 16:30:17 -0700

    this one was tough

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('KFC Fillet Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Maccies Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Supreme Burger', TRUE);
