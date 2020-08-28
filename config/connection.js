9efe94f87d9bbe1e3bbd3b548671ffc2623285ab
Mohamed Abdi <lovegoldone@gmail>
2020-08-27 17:23:08 -0700

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection + mysql.createConnection(peocess.env.JAWSDB_URL)
+}
+else {
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "0726329614",
     database: "burgers_db"
 });
+}
 
-
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
